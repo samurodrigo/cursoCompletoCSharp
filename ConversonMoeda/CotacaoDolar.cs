939ed70e5000415e9128ba60aead39ee6817f4d2
samuel r ferreira <samurodrigo@hotmail.com>
2019-06-28 06:45:27 -0300

    Conversor de moeda

---
+++ ConversonMoeda/CotacaoDolar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConversorMoeda
+{
+    class CotacaoDolar
+    {
+        static double IOF = 6.0;
+        public static double ConverteEmReais(double valorDolarEmReais, double quantidade)
+        {
+            double total =  valorDolarEmReais * quantidade;
+            return total + ((total * IOF) / 100);
+        }
+    }
+}
---
+++ ConversonMoeda/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConversorMoeda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar? ");
+            double cotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quatos dólares você vai comprar? ");
+            double quantidadeDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double total = CotacaoDolar.ConverteEmReais(cotacaoDolar, quantidadeDolar);
+
+            Console.WriteLine("Valor a ser pago em reais = " + total.ToString("F2", CultureInfo.InvariantCulture)); 
+        }
+    }
+}
